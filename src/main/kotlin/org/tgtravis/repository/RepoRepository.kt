3bfd3b04125e2ccf701063c58f1cf1a5f57443a4
Stanislav Myachenkov <s.myachenkov@gmail.com>
2018-06-08 00:16:02 +0300

    add findByName to RepoRepository

--- src/main/kotlin/org/tgtravis/repository/RepoRepository.kt
+++ src/main/kotlin/org/tgtravis/repository/RepoRepository.kt
@@ -2,5 +2,10 @@ package org.tgtravis.repository
 
 import org.springframework.data.repository.CrudRepository
 import org.tgtravis.model.Repo
+import java.util.Optional
 
-interface RepoRepository : CrudRepository<Repo, Long>
+interface RepoRepository : CrudRepository<Repo, Long> {
+
+    fun findByName(name: String): Optional<Repo>
+
+}
--- src/main/kotlin/org/tgtravis/repository/UserRepository.kt
+++ src/main/kotlin/org/tgtravis/repository/UserRepository.kt
@@ -2,10 +2,10 @@ package org.tgtravis.repository
 
 import org.springframework.data.repository.CrudRepository
 import org.tgtravis.model.User
-import java.util.*
+import java.util.Optional
 
 interface UserRepository : CrudRepository<User, Long> {
 
-    fun findByTelegramId(telegramId : Long) : Optional<User>
+    fun findByTelegramId(telegramId: Long): Optional<User>
 
 }
