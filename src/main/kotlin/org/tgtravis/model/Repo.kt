a1018cac5b3b0779174e5d7abf55fcc71ea7926d
Stanislav Myachenkov <s.myachenkov@gmail.com>
2018-06-08 16:49:01 +0300

    Implement custom hashCode and equals for Repo and User to avoid StackOverflow in Hibernate ManyToMany mapping

--- src/main/kotlin/org/tgtravis/model/Repo.kt
+++ src/main/kotlin/org/tgtravis/model/Repo.kt
@@ -1,5 +1,6 @@
 package org.tgtravis.model
 
+import java.util.Objects
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.GeneratedValue
@@ -18,8 +19,19 @@ data class Repo(
     var name: String = "",
 
     @ManyToMany(mappedBy = "repos", fetch = FetchType.EAGER)
-    var users: Set<User>? = HashSet()
+    var users: Set<User> = mutableSetOf<User>()
 
 ) {
-    constructor(name: String) : this(null, name, null)
+    constructor(name: String) : this(null, name, mutableSetOf())
+
+    override fun toString(): String = "$id $name"
+
+    override fun hashCode(): Int = Objects.hash(id, name)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (this::class !== other!!::class) return false
+        other as Repo
+        return this.id == other.id && this.name == other.name
+    }
 }
--- src/main/kotlin/org/tgtravis/model/User.kt
+++ src/main/kotlin/org/tgtravis/model/User.kt
@@ -1,5 +1,6 @@
 package org.tgtravis.model
 
+import java.util.Objects
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.GeneratedValue
@@ -28,8 +29,21 @@ data class User(
             joinColumns = [JoinColumn(name = "user_id")],
             inverseJoinColumns = [(JoinColumn(name = "repo_id"))]
     )
-    var repos: Set<Repo>? = HashSet()
+    var repos: Set<Repo> = mutableSetOf<Repo>()
 
 ) {
-    constructor(telegramId: Long, telegramUserName: String?) : this(null, telegramId, telegramUserName, null)
+    constructor(telegramId: Long, telegramUserName: String?) : this(null, telegramId, telegramUserName, mutableSetOf())
+
+    override fun toString(): String = "$id $telegramId $telegramUserName"
+
+    override fun hashCode(): Int = Objects.hash(id, telegramId, telegramUserName)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (this::class !== other!!::class) return false
+        other as User
+        return this.id == other.id
+                && this.telegramId == other.telegramId
+                && this.telegramUserName == other.telegramUserName
+    }
 }
