107f43e4742e60da2582ad7cace4746a3fc5717d
Stanislav Myachenkov <s.myachenkov@gmail.com>
2018-06-11 19:43:19 +0300

    update pom.xml

--- src/main/kotlin/org/tgtravis/TgTravis.kt
+++ src/main/kotlin/org/tgtravis/TgTravis.kt
@@ -1,13 +1,13 @@
 package org.tgtravis
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 import org.telegram.telegrambots.ApiContextInitializer
 
 @SpringBootApplication
-open class TgTravis
+class TgTravis
 
 fun main(args: Array<String>) {
     ApiContextInitializer.init()
-    SpringApplication.run(TgTravis::class.java, *args)
+    runApplication<TgTravis>(*args)
 }
