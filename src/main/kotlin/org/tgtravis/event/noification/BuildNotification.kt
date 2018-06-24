ce1141921c076b02d326d6ab5068f0459fcf3739
Stanislav Myachenkov <s.myachenkov@gmail.com>
2018-06-24 19:26:21 +0300

    update build notification

--- src/main/kotlin/org/tgtravis/event/noification/BuildNotification.kt
+++ src/main/kotlin/org/tgtravis/event/noification/BuildNotification.kt
@@ -8,8 +8,33 @@ class BuildNotification(
     private val notification: TravisNotification
 ) : BasicNotification(bot) {
 
+    private val messageTemplate = "New [build](%s) has arrived!\n\n" +
+            "Repository: %s\n" +
+            "Status: *%s*\n" +
+            "Result: *%s*\n" +
+            "Author: %s %s\n" +
+            "Committer: %s %s\n" +
+            "Committed at: *%s*\n" +
+            "Started at: %s\n" +
+            "Duration: %s\n" +
+            "Finished at: %s\n"
+
     override fun process() {
         val users = bot.service.retrieveUsers(notification.repoName)
-        notifyUsers(users, "New build has arrived! ${notification.payload.build_url}")
+        notifyUsers(users, String.format(messageTemplate,
+                notification.payload.build_url,
+                notification.repoName,
+                notification.payload.status_message,
+                notification.payload.result_message,
+                notification.payload.author_name,
+                notification.payload.author_email,
+                notification.payload.committer_name,
+                notification.payload.committer_email,
+                notification.payload.committed_at,
+                notification.payload.started_at,
+                notification.payload.duration,
+                notification.payload.finished_at
+            )
+        )
     }
 }
